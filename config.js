365e9b3eb3a48e8ded7dea55e7d5968b815525c0
alpha78130 <98257996+alpha78130@users.noreply.github.com>
2022-03-13 19:49:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['212653462056'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
